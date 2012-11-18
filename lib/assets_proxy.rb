e430c5f38bd25ea479514fd490b680a43aa9f235
Daniele Molteni <dani.m.mobile@gmail.com>
2012-11-18 09:20:39 +0100

    fix mimetype

--- lib/assets_proxy.rb
+++ lib/assets_proxy.rb
@@ -5,7 +5,7 @@ get '/stylesheets/*.css' do
 end
 
 get '/javascripts/*.js' do
-  content_type 'text/css', :charset => 'utf-8'
+  content_type 'text/javascript', :charset => 'utf-8'
   filename = params[:splat].first
   coffee filename.to_sym, :views => "#{settings.root}/public/javascripts"
 end
