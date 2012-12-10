97e017fb923abb614f11031edda8b02428ede582
Daniele Molteni <dani.m.mobile@gmail.com>
2012-12-10 12:02:49 +0100

    replace occurences in the TITLE tag

--- models/page.rb
+++ models/page.rb
@@ -52,13 +52,17 @@ class Page
     else
       body = original_content
       body.gsub! "<head>", "<head><base href=\"http://#{host}/\" target=\"_blank\">"
-      body.gsub! "</body>", "<script type=\"text/javascript\">\n var _gaq = _gaq || [];\n _gaq.push(['_setAccount', 'UA-5736692-22']);\n _gaq.push(['_trackPageview']);\n (function() {\n var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;\n ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';\n var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);\n })();\n </script>\n</body>"
+      body.gsub! "</body>", "#{google_analytics_code}\n</body>"
       doc = Nokogiri::HTML body
       each_text_node(doc) { |node| node.content = replace_occurrences_in(node.content) }
       html = doc.inner_html
       cache(html) if CACHE_PAGES
       html
     end
+  end
+
+  def google_analytics_code
+    "<script type=\"text/javascript\">\n var _gaq = _gaq || [];\n _gaq.push(['_setAccount', 'UA-5736692-22']);\n _gaq.push(['_trackPageview']);\n (function() {\n var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;\n ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';\n var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);\n })();\n </script>"
   end
 
   def cache html
@@ -72,11 +76,15 @@ class Page
   private
 
   def each_text_node html_doc, &block
-    html_doc.css('body *:not(script)').each do |tag|
+    html_doc.css('body *:not(script), head > title').each do |tag|
       tag.children.each do |node|
         yield(node) if node.text?
       end
     end
+  end
+
+  def replace_attribute_value tag_name, attribute_name
+    
   end
 
   def replace_occurrences_in string
